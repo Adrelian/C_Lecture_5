faee44940f354d20820ce1aa705df25752aca89c
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-25 20:06:12 +0500

    start working and end

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.Linq;
+int b; // такую запись инициализации переменной b не использовать
+var a = 12;
+Console.WriteLine(a.GetType());
+
+var data = new int[]{1,2,3,4}
+        .Where(e => e > 0)
+        .Select( e=> new {q = e, w = e + 1});
+Console.WriteLine(data.GetType().Name);
+
